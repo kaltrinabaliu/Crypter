87f1084fdc9332efa1e6078b0dbb5c979453ef7f
kaltrinabaliu <93614313+kaltrinabaliu@users.noreply.github.com>
2022-05-15 22:24:53 +0200

    https://github.com/kaltrinabaliu/Crypter.git

---
+++ src/model/Atbash.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Atbash extends Monoalphabetic {
+	
+	public Atbash() {
+	}
+
+	@Override
+	public String encrypt(String txt) {
+		
+		String crypter = "";
+		int index=0;
+		for (int i = 0; i < txt.length(); i++) {
+			index=searchLetter(0, txt.charAt(i));
+			if(index!=-1) {
+				if(index>=27) {
+					crypter+=ABC.charAt((ABC.length()-index)+(ABC.length()-1)/2);
+				}else {
+					crypter+=ABC.charAt(((ABC.length()/2)-1)-index);
+				}
+			}else {
+				crypter+=txt.charAt(i);
+			}
+		}return crypter;
+	}
+	
+	@Override
+	public String decrypt(String txt) {
+		
+		String decrypter= "";
+		
+		decrypter=encrypt(txt);
+		
+		return decrypter;
+	}
+	
+	private int searchLetter(int index, char letter){
+		
+		if(index<=ABC.length()-1) {
+			if(ABC.charAt(index)==letter) {
+				return index;
+			}else {
+				return searchLetter(index+1, letter);
+			}
+		}
+		return -1;
+	}
+}
